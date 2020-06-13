e6fcafa37e8a881ca173e8a5f71ec248bc489945
Brad Turner <brad@clanofartisans.com>
2020-06-13 17:50:25 -0500

    Add some config info for Travis CI.

--- config/database.php
+++ config/database.php
@@ -78,6 +78,18 @@ return [
             'prefix' => '',
         ],
 
+        'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'homestead'),
+            'username' => env('DB_USERNAME', 'homestead'),
+            'password' => env('DB_PASSWORD', 'secret'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+        ],
+
     ],
 
     /*
