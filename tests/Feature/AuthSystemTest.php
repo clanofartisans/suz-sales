aec13262bba6897ef088f144fcc18f181fc37d33
Brad Turner <brad@clanofartisans.com>
2020-06-15 20:31:28 -0500

    A couple very basic tests of the login functionality.

---
+++ tests/Feature/AuthSystemTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class AuthSystemTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withExceptionHandling();
+    }
+
+    /** @test */
+    function logging_in_with_valid_credentials()
+    {
+        $user = factory(User::class)->create([
+            'email'    => 'test@example.com',
+            'password' => Hash::make('Test Password')
+        ]);
+
+        $response = $this->post('/login', [
+            'email'    => 'test@example.com',
+            'password' => 'Test Password'
+        ]);
+
+        $response->assertRedirect('/');
+        $this->assertTrue(Auth::check());
+        $this->assertTrue(Auth::user()->is($user));
+    }
+
+    /** @test */
+    function login_attempt_with_invalid_credentials()
+    {
+        $user = factory(User::class)->create([
+            'email'    => 'test@example.com',
+            'password' => Hash::make('Test Password')
+        ]);
+
+        $response = $this->post('/login', [
+            'email'    => 'test@example.com',
+            'password' => 'wrong password'
+        ]);
+
+        $response->assertSessionHasErrors('email');
+        $this->assertFalse(Auth::check());
+    }
+
+    /** @test */
+    function login_attempt_with_account_that_does_not_exist()
+    {
+        $user = factory(User::class)->create([
+            'email'    => 'test@example.com',
+            'password' => Hash::make('Test Password')
+        ]);
+
+        $response = $this->post('/login', [
+            'email'    => 'nobody@example.com',
+            'password' => 'random password'
+        ]);
+
+        $response->assertSessionHasErrors('email');
+        $this->assertFalse(Auth::check());
+    }
+}
